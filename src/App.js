1482fa1a7a837e1dbe8bb4f935341640374b54da
NaDaPuPu <jun19998@gmail.com>
2021-03-01 03:30:18 +0900

    Using state(Practice)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,19 @@ import Subject from './components/Subject';
 import './App.css';
 
 class App extends Component {
+  constructor(props) { // render보다 일찍 실행해서 component의 초기화를 담당
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
