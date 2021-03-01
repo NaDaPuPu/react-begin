af3cbdcc624fb65f27139725ee624ac5aee9efa2
NaDaPuPu <jun19998@gmail.com>
2021-03-01 23:34:31 +0900

    ~ Chapter 17.1 Create Component event

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) { // render보다 일찍 실행해서 component의 초기화를 담당
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -16,15 +18,42 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  render() { // props나 state가 바뀌면 render가 다시 호출된다.
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
         </Subject>
+        {/* <header>
+          <h1><a href="/" onClick={function(e) {
+            console.log('event in', this);
+            e.preventDefault();
+            return;
+            console.log(e);
+            e.preventDefault();
+            // this.state.mode = 'welcome'; // 동적으로 state의 값을 변경할 때 이렇게 하면 안됨.
+            this.setState({
+              mode:'welcome'
+            });
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header> */}
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }
