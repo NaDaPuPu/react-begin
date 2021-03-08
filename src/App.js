0ebc7038acaf2a9ba129cb4580bb55d616ba192d
NaDaPuPu <jun19998@gmail.com>
2021-03-08 16:40:28 +0900

    Chapter 20.3 Change state

--- src/App.js
+++ src/App.js
@@ -52,24 +52,34 @@ class App extends Component {
         // var _contents = this.state.contents.concat(
         //   {id:this.max_content_id, title:_title, desc:_desc}
         // )
-        var newContents = Array.from(this.state.contents);
-        newContents.push({id:this.max_content_id, title:_title, desc:_desc});
+        var _contents = Array.from(this.state.contents);
+        _contents.push({id:this.max_content_id, title:_title, desc:_desc});
         this.setState({
-          contents:newContents
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
     } else if(this.state.mode === 'update') {
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc) {
-        this.max_content_id += 1;
-        var newContents = Array.from(this.state.contents);
-        newContents.push({id:this.max_content_id, title:_title, desc:_desc});
-        this.setState({
-          contents:newContents
-        });
-        console.log(_title, _desc);
-      }.bind(this)}></UpdateContent>
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+          var _contents = Array.from(this.state.contents);
+          var i = 0;
+          while(i < _contents.length) {
+            if(_contents[i].id === _id) {
+              _contents[i] = {id:_id, title:_title, desc:_desc};
+              break;
+            }
+            i += 1;
+          }
+          this.setState({
+            contents:_contents,
+            mode:'read'
+          });
+          console.log(_title, _desc);
+        }.bind(this)}></UpdateContent>
     }
     return _article;
   }
